5e1117cd84a60d8844643e598130ec56c0271438
Fouinkk <92111300+Fouinkk@users.noreply.github.com>
2021-10-07 22:31:03 -0400

    Update desktop.js

--- assets/desktop.js
+++ assets/desktop.js
@@ -71,7 +71,7 @@ document.querySelector(".note-pad").ondblclick = function () {
     document.querySelector(".window").classList.remove("minimized")
 }
 
-document.querySelector("textarea").value = "Hey Hustlers!\n\nLe party commence ici !\n\nConsignes:\n\n- Afin d'obtenir les informations concernant l'event ''FDB Fever'' vous devez venir consulter l'onglet ''Crevette Jr. - msn'' sporadiquement d'ici le 30 octobre 2021. Il est sur la tâche de trouver un sick party.\n\n- Toutes les icônes sur le bureau vous donne un accès exclusif à du contenu. Libre à vous d'aller voir ce qui se cache.\n\n- Cette fenêtre peut être fermé, réduite ou fermé.\n\nEnjoy\n\n"
+document.querySelector("textarea").value = "Hey Hustlers!\n\nLe party commence ici !\n\nConsignes:\n\n- Afin d'obtenir les informations concernant le party 'FDB Fever', vous devrez aller sur MSN sporadiquement d'ici le 30 octobre.\n\n- Toutes les icônes sur le bureau vous donnent un accès à du contenu exclusif.\n\n\nEnjoy\n\n"
 
 
 dragWindow(document.querySelector(".window"));
